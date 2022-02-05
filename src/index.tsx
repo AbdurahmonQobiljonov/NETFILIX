134dc08f73a3a4df58e50699c484d97babb544b2
Abdurakhmon Kobiljonov <abdurakhmon_kobiljonov@epam.com>
2022-02-05 17:59:50 +0500

    Project is started

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './style/style.scss';
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <h1>Hello wold</h1>
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
