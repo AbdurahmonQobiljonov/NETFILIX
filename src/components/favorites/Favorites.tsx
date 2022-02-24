f8a42ad1fa8b040002f63b3ce28d0b8ef8f6b33c
Abdurakhmon_Kobiljonov <xonobod.n@gmail.com>
2022-02-24 20:01:51 +0500

    add delete request

--- src/components/favorites/Favorites.tsx
+++ src/components/favorites/Favorites.tsx
@@ -9,7 +9,7 @@ import {IMovie} from "../../interfaces/interfaces";
 import {RootState} from "../../redux/store";
 
 const Favorites = () => {
-    const {error, loading, getAllMovies} = useNetflixService();
+    const {error, loading, getAllMovies, getMovieById} = useNetflixService();
     const dispatch = useDispatch();
     const {movies} = useSelector((state: RootState) => state.reducer);
     useEffect(() => {
@@ -20,6 +20,12 @@ const Favorites = () => {
         getAllMovies('GET').then(res => {
             // @ts-ignore
             dispatch(setMovies(res));
+        });
+    };
+
+    const onDelete = (id: string) => {
+        getMovieById(id, 'DELETE').then(res => {
+            console.log(res)
         });
     };
 
@@ -38,7 +44,6 @@ const Favorites = () => {
                     <div className="col-sm-12 overflow-hidden">
                         <div className="iq-main-header d-flex align-items-center justify-content-between">
                             <h4 className="main-title">Top Picks For You</h4>
-                            <a href="#" className="iq-view-all">View All</a>
                         </div>
                         <div className="favorite-contens">
                             <ul className="favorites-slider list-inline row p-0 mb-0">
@@ -46,51 +51,35 @@ const Favorites = () => {
                                     {movies?.map(({poster_path, title, id, vote_average}: IMovie) => (
                                             <li key={id} className="slide-item">
                                                 <div className="block-images position-relative">
-                                                    <Link to={`${id}`}>
-                                                        <div className="img-box">
-                                                            <img src={poster_path} className="img-fluid" alt=""/>
+                                                    <div className="img-box">
+                                                        <img src={poster_path} className="img-fluid" alt=""/>
+                                                    </div>
+                                                    <div className="block-description">
+                                                        <h6 className="iq-title">
+                                                            <a href="#"> {title} </a>
+                                                        </h6>
+                                                        <div className="movie-time d-flex align-items-center my-2">
+                                                            <div className="badge badge-secondary p-1 mr-2">16+</div>
+                                                            <span
+                                                                className="text-white">{vote_average + ' '}(imbd)</span>
                                                         </div>
-                                                        <div className="block-description">
-                                                            <h6 className="iq-title">
-                                                                <a href="#"> {title} </a>
-                                                            </h6>
-                                                            <div className="movie-time d-flex align-items-center my-2">
-                                                                <div className="badge badge-secondary p-1 mr-2">16+</div>
-                                                                <span
-                                                                    className="text-white">{vote_average + ' '}(imbd)</span>
-                                                            </div>
-                                                            <div className="hover-buttons">
-                                                                    <span className="btn btn-hover iq-button">
-                                                                      <i className="fa fa-play mr-1"></i>
-                                                                      Play Now
-                                                                    </span>
-                                                            </div>
+                                                        <div className="hover-buttons">
+                                                            <Link to={`${id}`} className="btn btn-hover iq-button">
+                                                                <i className="fa fa-play mr-1"></i>
+                                                                Play Now
+                                                            </Link>
                                                         </div>
-                                                        <div className="block-social-info">
-                                                            <ul className="list-inline p-0 m-0 music-play-lists">
-                                                                <li className="share">
-                                                                    <span><i className="fa fa-share-alt"></i></span>
-                                                                    <div className="share-box">
-                                                                        <div className="d-flex align-items-center">
-                                                                            <a href="#" className="share-ico"><i
-                                                                                className="fa fa-share-alt"></i></a>
-                                                                            <a href="#" className="share-ico"><i
-                                                                                className="fa fa-youtube"></i></a>
-                                                                            <a href="#" className="share-ico"><i
-                                                                                className="fa fa-instagram"></i></a>
-                                                                        </div>
-                                                                    </div>
-                                                                </li>
-                                                                <li>
-                                                                    <span><i className="fa fa-heart"></i></span>
-                                                                    <span className="count-box">19+</span>
-                                                                </li>
-                                                                <li>
-                                                                    <span><i className="fa fa-plus"></i></span>
-                                                                </li>
-                                                            </ul>
-                                                        </div>
-                                                    </Link>
+                                                    </div>
+                                                    <div className="block-social-info">
+                                                        <ul className="music-play-lists">
+                                                            <li className="share" onClick={() => onDelete(`/${id}`)}>
+                                                                <span>-</span>
+                                                            </li>
+                                                            <li>
+                                                                <span>+</span>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
                                                 </div>
                                             </li>
                                         )
