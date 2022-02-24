736bb4b9d000e158f8bbe8cfd13b7e29d614271f
Abdurakhmon_Kobiljonov <xonobod.n@gmail.com>
2022-02-24 13:40:09 +0500

    add ErrorBoundary for components

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,15 +3,24 @@ import Slider from "../components/main_slider/Slider";
 import Favorites from "../components/favorites/Favorites";
 import UpcomingMovie from "../components/upcoming_movie/UpcomingMovie";
 import TrendingSuggested from "../components/trending_suggested/TrendingSuggested";
+import ErrorBoundary from "../components/errorboundary/ErrorBoundary";
 
 const MainPage = ():JSX.Element => {
     return (
         <Fragment>
-            <Slider/>
+            <ErrorBoundary>
+                <Slider/>
+            </ErrorBoundary>
             <div className='main-content'>
-                <Favorites/>
-                <Favorites/>
-                <Favorites/>
+                <ErrorBoundary>
+                    <Favorites/>
+                </ErrorBoundary>
+                <ErrorBoundary>
+                    <Favorites/>
+                </ErrorBoundary>
+                <ErrorBoundary>
+                    <Favorites/>
+                </ErrorBoundary>
                 {/*<UpcomingMovie/>*/}
                 {/*<TrendingSuggested/>*/}
             </div>
