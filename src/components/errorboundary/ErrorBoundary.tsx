e57320c395627b9ee52ef01796d0e21e907f0c11
Abdurakhmon_Kobiljonov <xonobod.n@gmail.com>
2022-03-01 21:04:47 +0500

    change errorbaundary

--- src/components/errorboundary/ErrorBoundary.tsx
+++ src/components/errorboundary/ErrorBoundary.tsx
@@ -1,20 +1,20 @@
 import {Component} from 'react';
 
 class ErrorBoundary extends Component {
-    state:{hasError:boolean} = {
+    state: { hasError: boolean } = {
         hasError: false,
     };
 
-    static getDerivedStateFromError = (error: Error) => {
+    static getDerivedStateFromError(error: any) {
         return {hasError: true};
-    };
+    }
 
-    render(){
-       if (this.state.hasError) {
-           return <h1>Something went wrong.</h1>;
-       }
+    render() {
+        if (this.state.hasError) {
+            return <h1>Something went wrong.</h1>;
+        }
 
-       return this.props.children;
+        return this.props.children;
     }
 
 
