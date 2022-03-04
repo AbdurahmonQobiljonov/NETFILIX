9e9c2abd0a32c8217efd831251a30871734c3aac
Abdurakhmon_Kobiljonov <xonobod.n@gmail.com>
2022-03-04 08:22:13 +0500

    Add Error message

---
+++ src/components/errorMessage/ErrorMessage.tsx
@@ -0,0 +1,8 @@
+const ErrorMessage = (): JSX.Element => {
+    return <img
+        src='https://www.shareicon.net/data/128x128/2016/08/18/817711_error_512x512.png'
+        alt='Error'
+        style={{display:'block',width:'250px',height:'250px',objectFit:'contain',margin:'0 auto'}}
+    />
+}
+export default ErrorMessage;
