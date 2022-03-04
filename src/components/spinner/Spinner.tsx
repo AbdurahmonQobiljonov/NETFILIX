5ddcba1e6043891de4670308241775bb7a314afb
Abdurakhmon_Kobiljonov <xonobod.n@gmail.com>
2022-03-04 08:27:12 +0500

    update spinner change css

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -5,8 +5,10 @@ const Spinner = ():JSX.Element => {
     return <ClipLoader
         size={150}
         css={`
-            display: block;
-            margin: 0 auto;
+            display: block;\
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 40vh;
             border-color: red;`}
         color={'#123abc'}
         loading={true}/>
