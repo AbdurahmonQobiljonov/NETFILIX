f3961c25d653789bb0e2aec4113dd1ce3c02285c
Abdurakhmon_Kobiljonov <xonobod.n@gmail.com>
2022-03-13 16:07:04 +0500

    add type to actionCreators

--- src/redux/actions/actions.ts
+++ src/redux/actions/actions.ts
@@ -1,5 +1,6 @@
 import {createAction} from '@reduxjs/toolkit';
+import {IMovie} from "../../interfaces/interfaces";
 
-export const setMovies = createAction('SET_MOVIES');
-export const setMovie = createAction('SET_MOVIE');
-export const setActivePortal = createAction('SET_ACTIVE_PORTAL');
+export const setMovies = createAction<IMovie[]>('SET_MOVIES');
+export const setMovie = createAction<IMovie>('SET_MOVIE');
+export const setActivePortal = createAction<{type:string,payload:boolean}>('SET_ACTIVE_PORTAL');
